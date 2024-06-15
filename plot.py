e13d78f5162efddea88612778b872f275e2cd909
Jaewan Hong <dominico1004@hanmail.net>
2024-06-15 12:16:11 +0900

    .

--- plot.py
+++ plot.py
@@ -7,28 +7,22 @@ df2 = pd.read_csv('results/remote_write_bandwidth.csv')
 df3 = pd.read_csv('results/cxl_write_bandwidth.csv')
 
 # Plot the results
-fig, axs = plt.subplots(3, figsize=(10, 12))
+plt.figure(figsize=(10, 6))
 
 # Plot first dataframe
-axs[0].bar(df1['threads'], df1['bandwidth'])
-axs[0].set_title('Write Bandwidth vs Number of Threads - Dataframe 1')
-axs[0].set_xlabel('Number of Threads')
-axs[0].set_ylabel('Write Bandwidth (GB/s)')
-axs[0].grid(True)
+plt.plot(df1['threads'], df1['bandwidth'], label='Local Node')
 
 # Plot second dataframe
-axs[1].bar(df2['threads'], df2['bandwidth'])
-axs[1].set_title('Write Bandwidth on different memory access types with increasing number of threads ')
-axs[1].set_xlabel('Number of Threads')
-axs[1].set_ylabel('Write Bandwidth (GB/s)')
-axs[1].grid(True)
+plt.plot(df2['threads'], df2['bandwidth'], label='Remote Node')
 
 # Plot third dataframe
-axs[2].bar(df3['threads'], df3['bandwidth'])
-axs[2].set_title('Write Bandwidth vs Number of Threads - Dataframe 3')
-axs[2].set_xlabel('Number of Threads')
-axs[2].set_ylabel('Write Bandwidth (GB/s)')
-axs[2].grid(True)
+plt.plot(df3['threads'], df3['bandwidth'], label='CXL')
+
+plt.title('Write Bandwidth on different memory types with increasing number of threads')
+plt.xlabel('Number of Threads')
+plt.ylabel('Write Bandwidth (GB/s)')
+plt.legend()
+plt.grid(True)
 
 plt.tight_layout()
 plt.savefig('results/write_bandwidth.png')
